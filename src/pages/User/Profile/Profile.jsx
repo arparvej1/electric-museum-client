be71145051de99f8f1c4c6d45da346cc713ba510
arparvej1 <parvej8pro@gmail.com>
2024-08-16 11:51:34 +0600

    fix profile page bugs

--- src/pages/User/Profile/Profile.jsx
+++ src/pages/User/Profile/Profile.jsx
@@ -24,11 +24,11 @@ const Profile = () => {
         <h3 className='font-bold text-4xl'>My Profile</h3>
         <div className='flex flex-col md:flex-row items-center gap-5 bg-base-200 p-8 rounded-xl'>
           <div className=''>
-            <img className='w-32 h-32 rounded-full' src={user.photoURL || "https://i.ibb.co/ZT5tByN/avatar-15-blue.jpg"} alt={user.displayName} />
+            <img className='w-32 h-32 rounded-full' src={user?.photoURL || "https://i.ibb.co/ZT5tByN/avatar-15-blue.jpg"} alt={user?.displayName} />
           </div>
           <div className='flex flex-col gap-2'>
-            <h3 className='text-xl font-semibold'>{user.displayName}</h3>
-            <p>{user.email}</p>
+            <h3 className='text-xl font-semibold'>{user?.displayName}</h3>
+            <p>{user?.email}</p>
           </div>
           <div className='text-right  navbar-end'>
             <Link to='/update-profile' className='text-xl font-semibold underline btn btn-ghost text-blue-600'>Edit Profile</Link>
@@ -41,11 +41,11 @@ const Profile = () => {
             <div className='flex flex-col gap-3 md:text-xl'>
               <div>
                 <p>Full Name:</p>
-                <p className=' font-semibold'>{user.displayName || 'Full Name Not Found!'}</p>
+                <p className=' font-semibold'>{user?.displayName || 'Full Name Not Found!'}</p>
               </div>
               <div>
                 <p>Email:</p>
-                <p className=' font-semibold'>{user.email}</p>
+                <p className=' font-semibold'>{user?.email}</p>
               </div>
             </div>
           </div>
@@ -55,11 +55,11 @@ const Profile = () => {
             <div className='flex flex-col gap-3 md:text-xl'>
               <div>
                 <p>Account Creation Date & Time:</p>
-                <p className=' font-semibold'>{user.metadata.creationTime}</p>
+                <p className=' font-semibold'>{user?.metadata.creationTime}</p>
               </div>
               <div>
                 <p>Last Login:</p>
-                <p className=' font-semibold'>{user.metadata.lastSignInTime}</p>
+                <p className=' font-semibold'>{user?.metadata.lastSignInTime}</p>
               </div>
             </div>
           </div>
