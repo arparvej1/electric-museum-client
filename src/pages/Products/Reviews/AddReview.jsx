4a0fe12974e7fc777cb907f6eb1fbfc61bf21ebc
arparvej1 <parvej8pro@gmail.com>
2024-08-17 23:38:35 +0600

    add myReviews

--- src/pages/Products/Reviews/AddReview.jsx
+++ src/pages/Products/Reviews/AddReview.jsx
@@ -4,43 +4,40 @@ import ReactStars from "react-rating-stars-component";
 import useAuth from '../../../hooks/useAuth';
 import LoginModel from '../../User/Login/LoginModel';
 import PropTypes from 'prop-types';
-import useAxiosSecure from '../../../../backup/hooks/useAxiosSecure';
+import axios from 'axios';
 
 const AddReview = ({ product }) => {
   const { user } = useAuth();
-  const axiosSecure = useAxiosSecure();
   const { _id } = product;
   // ---------------- review start ---------------------
 
-  // const [reviews, setReviews] = useState([]);
-  // const loadReview = async () => {
-  //   try {
-  //     const response = await axiosSecure.get(`/myReviews/${user.email}`);
-  //     // console.log('reviews', response.data);
-  //     setReviews(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const [reviews, setReviews] = useState([]);
+  const [myReviews, setMyReviews] = useState([]);
 
-  // useEffect(() => {
-  //   loadReview();
-  // }, []);
+  const loadReview = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_VERCEL_API}/reviews/${_id}`);
+      console.log('reviews', response.data);
+      setReviews(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // const [thisScholarship, setThisScholarship] = useState([]);
+  const loadMyReview = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_VERCEL_API}/myReviews?email=${user?.email}&productId=${_id}`);
+      console.log('myReviews', response.data);
+      setMyReviews(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await axiosSecure.get(`/scholarship/${scholarshipId}`);
-  //       setThisScholarship(res.data);
-  //       // console.log('single scholarship', res.data);
-  //     } catch (error) {
-  //       console.error('Error fetching applied scholarships:', error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    loadReview();
+    loadMyReview();
+  }, [user]);
 
   const [reviewRating, setReviewRating] = useState(0);
   const [ratingMsg, setRatingMsg] = useState('');
@@ -82,12 +79,13 @@ const AddReview = ({ product }) => {
 
     console.log(completeReview);
     // --------- send server start ----- 
-    await axiosSecure.post(`/reviews`, completeReview)
+    await axios.post(`${import.meta.env.VITE_VERCEL_API}/reviews`, completeReview)
       .then(function (response) {
         console.log(response.data);
         if (response.data.acknowledged) {
           toast.success('Thanks for Review!');
-          // loadReview();
+          loadReview();
+          loadMyReview();
           form.reset();
         }
       })
@@ -100,39 +98,70 @@ const AddReview = ({ product }) => {
 
   return (
     <div>
-      <h3 className="font-bold text-lg text-center">Review</h3>
-      <form
-        onSubmit={handleAddReview}
-        className="flex flex-col gap-5 w-full md:w-3/5 lg:w-1/2 mx-auto">
-        <div className="grid grid-cols-1 gap-5">
-          <label className="flex flex-col gap-1 w-full">
-            <span>Comment</span>
-            <textarea name="comment" placeholder="Write your review here" className="textarea textarea-bordered h-24 w-full" required ></textarea>
-          </label>
-          <label className="flex gap-1 w-full items-center">
-            <span>Rating: </span>
-            <ReactStars
-              count={5}
-              onChange={ratingChanged}
-              size={24}
-              emptyIcon={<i className="far fa-star"></i>}
-              halfIcon={<i className="fa fa-star-half-alt"></i>}
-              fullIcon={<i className="fa fa-star"></i>}
-              activeColor="#ffd700"
-            />
-          </label>
-          {ratingMsg && <p className="text-red-500">{ratingMsg}</p>}
-          <label className="flex flex-col gap-1 w-full">
-            <span>Review Date</span>
-            <input type="date" name="reviewDate" value={new Date().toISOString().substring(0, 10)} className="input input-bordered w-full" required />
-          </label>
-        </div>
-        <div className="gap-5">
-          <label className="flex flex-col gap-1 w-full">
-            <input type="submit" value="Submit" className="btn bg-secondary text-secondary-content w-full" />
-          </label>
-        </div>
-      </form>
+      {
+        myReviews.length > 0 ?
+          <div className='w-full md:w-3/5 lg:w-1/2 mx-auto'>
+            <h3 className="font-bold text-lg text-center mb-2">My Review</h3>
+            <div className="grid grid-cols-1 gap-5">
+              <label className="flex flex-col gap-1 w-full">
+                <p>{myReviews[0].comment}</p>
+              </label>
+              <label className="flex gap-1 w-full items-center">
+                <span className="font-semibold">Rating: </span>
+                <ReactStars
+                  count={5}
+                  value={myReviews[0].rating} // Set the fixed rating value here
+                  size={24}
+                  edit={false} // Disable editing
+                  emptyIcon={<i className="far fa-star"></i>}
+                  halfIcon={<i className="fa fa-star-half-alt"></i>}
+                  fullIcon={<i className="fa fa-star"></i>}
+                  activeColor="#ffd700"
+                />
+              </label>
+              <label>
+                <span className="font-semibold">Review Date: </span>
+                <span>{myReviews[0].reviewDate}</span>
+              </label>
+            </div>
+          </div>
+          :
+          <div>
+            <h3 className="font-bold text-lg text-center">Review</h3>
+            <form
+              onSubmit={handleAddReview}
+              className="flex flex-col gap-5 w-full md:w-3/5 lg:w-1/2 mx-auto">
+              <div className="grid grid-cols-1 gap-5">
+                <label className="flex flex-col gap-1 w-full">
+                  <span>Comment</span>
+                  <textarea name="comment" placeholder="Write your review here" className="textarea textarea-bordered h-24 w-full" required ></textarea>
+                </label>
+                <label className="flex gap-1 w-full items-center">
+                  <span>Rating: </span>
+                  <ReactStars
+                    count={5}
+                    onChange={ratingChanged}
+                    size={24}
+                    emptyIcon={<i className="far fa-star"></i>}
+                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                    fullIcon={<i className="fa fa-star"></i>}
+                    activeColor="#ffd700"
+                  />
+                </label>
+                {ratingMsg && <p className="text-red-500">{ratingMsg}</p>}
+                <label className="flex flex-col gap-1 w-full">
+                  <span>Review Date</span>
+                  <input type="date" name="reviewDate" value={new Date().toISOString().substring(0, 10)} className="input input-bordered w-full" required />
+                </label>
+              </div>
+              <div className="gap-5">
+                <label className="flex flex-col gap-1 w-full">
+                  <input type="submit" value="Submit" className="btn bg-secondary text-secondary-content w-full" />
+                </label>
+              </div>
+            </form>
+          </div>
+      }
       {/* ------- Login Form Start ---------- */}
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
       <dialog id="login_modal" className="modal">
